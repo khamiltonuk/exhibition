ecdb0b5fa8a5394b04263c2e22dc9ca34e7fae4c
beta <beta.nycz@icloud.com>
2020-08-05 20:00:06 +0200

    artists page basic layout

--- src/pages/events.js
+++ src/pages/events.js
@@ -1,32 +1,32 @@
-import React from "react"
-import Scene from "./Scene"
-import AnotherScene from "./AnotherScene"
-import OneMoreScene from "./OneMoreScene"
+import React from 'react';
+import Scene from './Scene';
+import AnotherScene from './AnotherScene';
+import OneMoreScene from './OneMoreScene';
 
-import Layout from "../components/layout"
+import Layout from '../components/layout';
 
 const EventPage = () => (
-  <Layout>
-    <h1>Event pages</h1>
-    <img scr="../myimage" alt="hello picture" />
-    <Scene />
-    <AnotherScene />
-    <OneMoreScene />
-    <nav className="mainNav | visually-hidden">
-      <ul>
-        <li>
-          <a href="#">Watermelon</a>
-        </li>
-        <li>
-          <a href="#">Banana</a>
-        </li>
-        <li>
-          <a href="#">Strawberry</a>
-        </li>
-      </ul>
-    </nav>
-    <canvas id="stage"></canvas>
-  </Layout>
-)
+	<Layout>
+		<h1>Event pages</h1>
+		<img scr="../myimage" alt="" />
+		<Scene />
+		<AnotherScene />
+		<OneMoreScene />
+		<nav className="mainNav | visually-hidden">
+			<ul>
+				<li>
+					<a href="#Watermelon">Watermelon</a>
+				</li>
+				<li>
+					<a href="#Banana">Banana</a>
+				</li>
+				<li>
+					<a href="#Banana">Strawberry</a>
+				</li>
+			</ul>
+		</nav>
+		<canvas id="stage" />
+	</Layout>
+);
 
-export default EventPage
+export default EventPage;
