ef482f8d718ea5412f5276b3dbe2844c8972a413
Kristian <kristian.hamilton@signavio.com>
2020-08-26 10:19:29 +0200

    fix errors

--- src/pages/events.js
+++ src/pages/events.js
@@ -8,20 +8,20 @@ import Layout from "../components/layout"
 const EventPage = () => (
   <Layout>
     <h1>Event pages</h1>
-    <img scr="../myimage" alt="hello picture" />
+    <img scr="../myimage" alt="hello" />
     <Scene />
     <AnotherScene />
     <OneMoreScene />
     <nav className="mainNav | visually-hidden">
       <ul>
         <li>
-          <a href="#">Watermelon</a>
+          <a href="/">Watermelon</a>
         </li>
         <li>
-          <a href="#">Banana</a>
+          <a href="/">Banana</a>
         </li>
         <li>
-          <a href="#">Strawberry</a>
+          <a href="/">Strawberry</a>
         </li>
       </ul>
     </nav>
